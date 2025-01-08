d7f2e446ccd63bedb2baca4b06163e00d0415353
kimura-keisuke-1227 <kim.ksuke@gmail.com>
2025-01-08 17:53:30 +0900

    最初の実装

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 from MyLogger import MyLogger
+import os
+
+
+if __name__ == '__main__':
+    print('Program begin!')
+    logger = MyLogger().logger
+    logger.info(f'Program Start!')
+    logger.info(f'Program End!')
+    print('Program End!')
