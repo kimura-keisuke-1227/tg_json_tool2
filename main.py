990166315b62f8c31b7afe497f925bca98a339d8
kimura-keisuke-1227 <kim.ksuke@gmail.com>
2025-01-08 17:57:35 +0900

    ログファイル名を追加

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 
 if __name__ == '__main__':
     print('Program begin!')
-    logger = MyLogger().logger
+    logger = MyLogger("TaglotJsonMaker").logger
     logger.info(f'Program Start!')
     logger.info(f'Program End!')
     print('Program End!')
